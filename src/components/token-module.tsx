7ad1f51c305b61c459d1d40bd2170f1fa48ed2ad
Roberto Ríos <roberto@deveahub.com>
2024-01-22 12:10:25 +0100

    fix: styles

--- src/components/token-module.tsx
+++ src/components/token-module.tsx
@@ -1,35 +1,33 @@
-import useControls from '@/pods/controls/hooks/use-controls';
-import useGeneratorOptions from '@/pods/crypto/hooks/use-generator-options';
-import CopyButton from './copy-button';
+import useControls from "@/pods/controls/hooks/use-controls";
+import CopyButton from "./copy-button";
 
 type TokenModuleProps = {
-	id: number;
-	tokens: string[];
+  id: number;
+  tokens: string[];
 };
 
-const TokenModule = ({id, tokens}: TokenModuleProps) => {
-	const {showTokens} = useControls();
-	const options = useGeneratorOptions();
+const TokenModule = ({ id, tokens }: TokenModuleProps) => {
+  const { showTokens } = useControls();
 
-	return (
-		<div className='flex flex-col gap-2 items-start'>
-			<CopyButton text={JSON.stringify(tokens)} className='text-xl'>
+  return (
+    <div className="flex flex-col gap-2 items-start max-w-full">
+      <CopyButton text={JSON.stringify(tokens)} className="text-xl">
         Module {id + 1}
-			</CopyButton>
-			{tokens.map((token, idx) => (
-				<div className='flex gap-2 items-center w-full' key={`${idx}-${token}`}>
-					{idx + 1}
-					<CopyButton
-						text={token}
-						className='text-xs overflow-x-hidden whitespace-nowrap overflow-hidden overflow-ellipsis'
-						key={token}
-					>
-						{showTokens ? token : token.replace(/./g, '*')}
-					</CopyButton>
-				</div>
-			))}
-		</div>
-	);
+      </CopyButton>
+      {tokens.map((token, idx) => (
+        <div className="flex gap-2 items-center w-full" key={`${idx}-${token}`}>
+          {idx + 1}
+          <CopyButton
+            text={token}
+            className="text-xs overflow-x-hidden whitespace-nowrap overflow-hidden overflow-ellipsis"
+            key={token}
+          >
+            {showTokens ? token : token.replace(/./g, "*")}
+          </CopyButton>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TokenModule;
