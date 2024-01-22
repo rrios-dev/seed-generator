d339b4ca4966bc4324c85cd8d4dd4407b4e6654e
Roberto Ríos <roberto@deveahub.com>
2024-01-22 21:28:41 +0100

    test: add vitest and crypto test

--- src/pods/crypto/crypto.spec.ts
+++ src/pods/crypto/crypto.spec.ts
@@ -1,36 +1,32 @@
+import { expect, test } from 'vitest';
 import { initialOptions } from './atoms/options';
 import { makeModules } from './crypto';
 
-describe('crypto', () => {
-    it.each(['seed-test', 'test-1', 'main-seed', 'secretSeed with parameters'])(
-        'seed crypto generations %s',
-        (seed) => {
+['seed-test', 'test-1', 'main-seed', 'secretSeed with parameters'].forEach(
+    (seed) => {
+        test(`seed crypto generations ${seed}`, () => {
             const result = makeModules({
                 ...initialOptions,
                 seed,
-                modulesCount: 1,
-                tokenLength: 10,
-                tokensByModule: 1,
             });
 
             expect(result).toMatchSnapshot();
-        }
-    );
+        });
+    }
+);
 
-    // It.each([
-    // 	['seed-test', 12, 10, 10],
-    // 	['seed-test', 12, 10, 10],
-    // ])(
-    // 	'seed crypto generations with custom parameters',
-    // 	(seed, modulesCount, tokenLength, tokensByModule) => {
-    // 		const result = makeModules({
-    // 			modulesCount,
-    // 			tokenLength,
-    // 			tokensByModule,
-    // 			seed,
-    // 		});
+[
+    ['seed-test', 12, 10, 10],
+    ['seed-test', 10, 14, 10],
+].forEach(([seed, modulesCount, tokenLength, tokensByModule]) => {
+    test(`seed crypto generations ${seed} with custom parameters`, () => {
+        const result = makeModules({
+            modulesCount: modulesCount as number,
+            tokenLength: tokenLength as number,
+            tokensByModule: tokensByModule as number,
+            seed: seed as string,
+        });
 
-    // 		expect(result).toMatchSnapshot();
-    // 	},
-    // );
+        expect(result).toMatchSnapshot();
+    });
 });
